34e94478864b9cc213be704507282d673da567cd
Chandravadan Subraveti <csubraveti@atlassian.com>
2019-08-17 16:05:28 +1000

    Add emacs config

---
+++ emacs/.emacs.d/init.el
@@ -0,0 +1,7 @@
+;;; Package management
+(require 'package)
+
+(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
+			 ("marmalade" . "https://marmalade-repo.org.packages/")
+			 ("melpa" . "https://melpa.org/packages/"))
+ 
