50e3e5dc69d010a2bca1da488cacc2194c73fecb
Chandravadan S <s.chandravadan@gmail.com>
2019-12-30 08:30:58 +1100

    Add bindings to move workspaces to monitors, xrandr improvements

--- i3/.config/i3/setup-displays.sh
+++ i3/.config/i3/setup-displays.sh
@@ -22,11 +22,11 @@ prev=$(last_state)
 current=$(current_state)
 
 if [ $current -eq 1 ]; then
-				xrandr --output $LAPTOP --mode $MODE_1080p
+				xrandr --output $LAPTOP --primary --mode $MODE_1080p
 elif [ $current -eq 2 ]; then
-				xrandr --output $DESK --mode $MODE_1080p --pos 0x0 --output $LAPTOP --mode $MODE_1080p --right-of $DESK
+				xrandr --output $DESK --primary --mode $MODE_1080p --pos 0x0 --output $LAPTOP --mode $MODE_1080p --right-of $DESK
 elif [ $current -eq 3 ]; then
-				xrandr --output $WORK1 --mode $MODE_1080p --pos 0x0 --output $WORK2 --mode $MODE_1080p --right-of $WORK1 --output $LAPTOP --below $WORK1 --mode $MODE_1080p
+				xrandr --output $WORK1 --primary --mode $MODE_1080p --pos 0x0 --output $WORK2 --mode $MODE_1080p --right-of $WORK1 --output $LAPTOP --right-of $WORK2 --mode $MODE_1080p
 else 
 				notify-send "$current laptop setup not scripted yet, falling back to laptop setup"
 				xrandr --output $LAPTOP --mode $MODE_1080p
@@ -56,6 +56,9 @@ case $prev in
                         1)
 																i3-msg '[class=".*"]' move workspace to output $LAPTOP
                                 ;;
+												2) 		
+																# no-op
+																;;
                         3)
                                 notify-send "Switching over from 2 connected displays to 3 not supported"
 																i3-msg '[class=".*"]' move workspace to output $LAPTOP
@@ -76,6 +79,9 @@ case $prev in
                                 notify-send "Switching over from 3 connected displays to 2 not supported"
 																i3-msg '[class=".*"]' move workspace to output $LAPTOP
                                 ;;
+												3)
+																#no-op
+																;;
                         *)
                                 notify-send "Connected $current displays. Whoa!"
 																i3-msg '[class=".*"]' move workspace to output $LAPTOP
